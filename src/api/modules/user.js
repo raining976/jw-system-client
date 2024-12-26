fb9cef744ab1697a8a2b89e27abe04e8da1c42b6
lll <1013336369@qq.com>
2024-12-26 22:13:03 +0800

    feat:添加个人信息页面

--- src/api/modules/user.js
+++ src/api/modules/user.js
@@ -1,3 +1,4 @@
 import { get, post } from '@/utils/request'
 
-export const getUserAll = (data = {})=>post('/user/getAll', data, false)
+export const getUserAll = (data = {}) => post('/user/getAll', data, false)
+export const getUserInfo = (data = {}) => get('/user/info', data, false)
