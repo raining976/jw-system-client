c2c0018f13a4f116ba3854fbe88777961ac1b2e1
raining <running.zhang@foxmail.com>
2024-12-27 20:31:50 +0800

    feat:增加课程相关接口

--- src/api/modules/course.js
+++ src/api/modules/course.js
@@ -2,3 +2,5 @@ import { get, post } from '@/utils/request'
 
 export const findCourse = (data) => get('/course/find', data, false)
 export const deleteCourses = (data) => post('/course/delete', data, true)
+export const createCourse = (data) => post('/course/add', data, true)
+export const updateCourse = (data) => post('/course/update', data, true)
