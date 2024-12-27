3f46f80db1b3dc606a625e62e4ff92ec93a794c2
lll <1013336369@qq.com>
2024-12-28 00:14:26 +0800

    feat:完成学生和老师的增删改查

--- src/views/userListView/components/teacher.vue
+++ src/views/userListView/components/teacher.vue
@@ -9,15 +9,17 @@
             <button class="btn btn-primary" style="width: 120px;" @click="fetchTeachers">搜索</button>
         </div>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            添加老师
-        </button>
+        <el-button plain @click="dialogVisible = true, isEdit = false">
+            添加教师
+        </el-button>
     </div>
     <div>
         <myTable :data="teachers" :columns="columns" :current-page="currentPage" :total-pages="totalPages"
             @change-page="handlePageChange" @edit="handleEdit" @delete="handleDelete" selectable editable deletable
             @delete-selected="handleDeleteSelected" />
     </div>
+    <common-form-dialog v-model:visible="dialogVisible" title="教师信息" :fields="formFields" :modelValue="form"
+        :isEdit="isEdit" @submit="handleSubmit" @update:visible="updateVisible" @close="closeDialog" :rules="rules" />
 </template>
 
 <script setup>
@@ -32,7 +34,7 @@ const columns = ref([
     { key: "user_id", label: "用户id" },
     { key: "username", label: "用户名" },
     { key: "teacher_id", label: "教师工号" },
-    { key: "name", label: "教师姓名" },
+    { key: "teacher_name", label: "教师姓名" },
     { key: "gender", label: "性别" },
     { key: "department", label: "院系" },
 ]);
@@ -42,9 +44,6 @@ const handleDelete = async (row) => {
     fetchTeachers()
 };
 
-const handleEdit = (row) => {
-    alert(`Edit teachers: ${row.name}`);
-};
 
 const handleDeleteSelected = async (selectedItems) => {
     const ids = selectedItems.map((item) => item.user_id);
@@ -69,17 +68,15 @@ const { proxy } = getCurrentInstance()
 const fetchTeachers = async () => {
     // 调用后端分页接口
     let form = {
-        role: 'teacher',
+        // role: 'teacher',
         pageSize: pageSize.value,
         pageNum: currentPage.value,
         keyword: searchValue.value,
     }
-    await proxy.$api.getUserAll(form).then(res => {
-        console.log(res)
+    await proxy.$api.getTeacherAll(form).then(res => {
         if (res.code == 20000) {
             teachers.value = res.data.data
             totalPages.value = Math.ceil(res.data.total / pageSize.value)
-            console.log('totalPages.value', totalPages.value)
         } else {
             errorNotice(res.msg)
         }
@@ -91,6 +88,99 @@ const fetchTeachers = async () => {
 onMounted(() => {
     fetchTeachers()
 })
+
+// 以下是编辑和添加部分
+const handleEdit = (row) => {
+    isEdit.value = true
+    dialogVisible.value = true
+    form.value = row
+};
+const dialogVisible = ref(false)
+const isEdit = ref(false)
+const formFields = ref([
+    { label: "用户名", prop: "username", type: "el-input", disabled: true },
+    { label: "教师工号", prop: "teacher_id", type: "el-input" },
+    { label: "教师姓名", prop: "teacher_name", type: "el-input" },
+    {
+        label: "性别",
+        prop: "gender",
+        type: "el-select",
+        attrs: {
+            options: [
+                {
+                    label: "男",
+                    value: "男",
+                },
+                {
+                    label: "女",
+                    value: "女",
+                },
+                {
+                    label: "保密",
+                    value: "保密",
+                },
+            ]
+        }
+    },
+    { label: "院系", prop: "department", type: "el-input" },
+])
+
+
+const emptyForm = ref({ // 空表 用来恢复form
+    user_id: '',
+    username: "",
+    teacher_id: "",
+    teacher_name: "",
+    gender: "",
+    department: "",
+})
+
+const form = ref({
+    user_id: '',
+    username: "",
+    teacher_id: "",
+    teacher_name: "",
+    gender: "",
+    department: "",
+})
+
+const rules = ref({
+    username: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+    teacher_id: [{ required: true, message: "学号不能为空", trigger: "blur" }],
+    teacher_name: [{ required: true, message: "教师姓名不能为空", trigger: "blur" }],
+    gender: [{ required: true, message: "性别不能为空", trigger: "blur" }],
+    department: [{ required: true, message: "院系不能为空", trigger: "blur" }],
+});
+
+const handleSubmit = (form) => {
+    isEdit.value ? updateTeacher(form) : createTeacher(form)
+}
+
+
+const createTeacher = async (form) => {
+    const { username, teacher_id, teacher_name, gender, department } = form
+    await proxy.$api.createTeacher({ username, teacher_id, teacher_name, gender, department }).then(res => {
+        fetchTeachers()
+    })
+
+}
+
+const updateTeacher = async (form) => {
+    const { user_id, username, teacher_id, teacher_name, gender, department } = form
+    await proxy.$api.updateTeacherInfo({ user_id, username, teacher_id, teacher_name, gender, department }).then(res => {
+        fetchTeachers()
+    })
+
+}
+
+const updateVisible = (v) => {
+    dialogVisible.value = v
+}
+
+const closeDialog = () => {
+    dialogVisible.value = false
+    form.value = { ...emptyForm.value }
+}
 
 
 
